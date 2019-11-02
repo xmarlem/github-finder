7adb9ed6ce072f0fed076538a8f9811987c8619f
Marco Lembo <marco@MacBook-Pro-di-Marco.local>
2019-11-02 22:51:59 +0100

    netlify enabled

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -11,6 +11,18 @@ import {
 } from '../types';
 
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
+
 const GithubState = props => {
     const initialState = {
         users: [],
@@ -26,8 +38,7 @@ const GithubState = props => {
         setLoading();
         const res = 
           await axios.get(`https://api.github.com/search/users?q=${text}&
-          client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-          {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+          client_id=${githubClientId}&client_secret=${githubClientSecret}`)
     
         dispatch({
             type: SEARCH_USERS,
@@ -42,8 +53,7 @@ const GithubState = props => {
         setLoading()
         const res = 
         await axios.get(`https://api.github.com/users/${username}?
-        client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-        {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+        client_id=${githubClientId}&client_secret=${githubClientSecret}`)
 
         dispatch({
             type: GET_USER,
@@ -57,8 +67,7 @@ const GithubState = props => {
         setLoading()
         const res = 
             await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created_asc&
-            client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-            {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+            client_id=${githubClientId}&client_secret=${githubClientSecret}`)
 
         dispatch({
             type: GET_REPOS,
