7e530e5df355701518933fee36bcc6888ef4f850
Marco Lembo <marco@MacBook-Pro-di-Marco.local>
2019-11-02 14:52:27 +0100

    not found (2)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/User'
@@ -8,37 +8,33 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import About from './components/pages/About'
 import Profile from './components/pages/Profile'
 import Home from './components/pages/Home'
-
+import NotFound from './components/pages/NotFound'
 
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
 
 
 const App = () => {
-
   return (
-    <Fragment>
-      <GithubState>
-        <AlertState>
-          <Router>
-            <div className="App">
-              <Navbar />
-              <div className="container">
-                <Alert />
-                <Switch>
-                  <Route exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/user/:login' component={User} />
-                  <Route exact path='/profile' component={Profile} />
-                </Switch>
-              </div>
-            </div> 
-          </Router>
-        </AlertState>
-      </GithubState>
-
-    </Fragment>
-
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route exact path='/profile' component={Profile} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div> 
+        </Router>
+      </AlertState>
+    </GithubState>
   );  
 }
 
